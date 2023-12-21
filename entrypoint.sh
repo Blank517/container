01c893067311d187a518c02367dcf3e1315504f4
Blank517 <nico.taddei2@gmail.com>
2023-12-21 13:59:16 +0100

    Use bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-
-cd /opt/graalvm-community-openjdk-17.0.9+9.1
-
-ls -la
-
-
-cd /opt/graalvm-community-openjdk-17.0.9+9.1/bin
-
-ls -la
-
 # Switch to the container's working directory
 cd /home/container || exit 1
-
-echo "${PATH}"
-echo "${JAVA_HOME}"
-
-
 
 # Print Java version
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
